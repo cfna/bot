7e3bc5a9829ca8ed1a19ac36b9dfab4817389641
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-12-07 23:51:08 +0100

    Updated: dependencies, scripts & run actions. Added: sample script files

---
+++ src/run.ts
@@ -0,0 +1,34 @@
+import { CFBot, ScreenController } from '.';
+import robot from 'robotjs';
+import { createLogger, Logger } from './utils';
+
+function run(): void {
+  const bot = new CFBot();
+  const sc = (bot.getController('screen') as ScreenController);
+  sc.printScreenInfo();
+  sc.capture(0, 0, 800, 600);
+}
+
+let prevPos = { x: 0, y: 0 };
+
+function queryMousePosition() {
+  const logger = createLogger();
+  getMousePos(logger);
+}
+
+function getMousePos(logger: Logger) {
+  setTimeout(() => {
+    const position = robot.getMousePos();
+    if(position && position.x !== prevPos.x && position.y !== prevPos.y) {
+      logger.info(`Mouse Position: x=${position.x} y=${position.y}`);
+      prevPos = position;
+    }
+    getMousePos(logger);
+  }, 1000);
+}
+
+// run();
+if(false) {
+  run();
+}
+queryMousePosition();
