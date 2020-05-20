7b7f2aefd2acc9e0ee1d5506f20ff89331350864
Patrick Jung <patrick.pddstudio@gmail.com>
2020-05-20 16:22:16 +0200

    tmp: Added todos to remember where to continue working

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,7 @@
 export * from './mouse-controller';
 export * from './screen-controller';
 export * from './keyboard-controller';
+
+// TODO: Add pre execution checks for parameter validation
+// TODO: Add possible fallback values for missing parameters
+// TODO: Add new supported keys from robotjs
