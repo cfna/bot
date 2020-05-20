edbe6085c4b14700a3c6f25e1a14a0e97f02e23b
Patrick Jung <patrick.pddstudio@googlemail.com>
2020-05-20 22:24:51 +0200

    Moved todos to tickets

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,7 +1,3 @@
-export * from './mouse-controller';
-export * from './screen-controller';
-export * from './keyboard-controller';
-
-// TODO: Add pre execution checks for parameter validation
-// TODO: Add possible fallback values for missing parameters
-// TODO: Add new supported keys from robotjs
+export * from './mouse-controller'
+export * from './screen-controller'
+export * from './keyboard-controller'
