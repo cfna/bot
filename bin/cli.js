fe0b4d11d11feee6cab0675d22a403dc1573c24a
Patrick Jung <patrick.pddstudio@googlemail.com>
2020-10-04 14:36:45 +0200

    #14: Added support to print mouse positions in terminal

--- bin/cli.js
+++ bin/cli.js
@@ -2,12 +2,28 @@
 
 const yargs = require('yargs')
 const path = require('path')
+const readline = require('readline')
 const { MacroController } = require('../dist')
 const { ScriptLoader } = require('../dist/utils')
+const { runDebug } = require('../dist/runDebug')
 
-const argv = yargs.option('macro', {alias: 'm', type: 'string' }).argv
+const argv = yargs.option('macro', {
+  alias: 'm', 
+  type: 'string',
+  description: 'Specify the macro file to execute' 
+}).option('debug', {
+  alias: 'd', 
+  type: 'number',
+  description: 'Debug: Print mouse coordinates in console. Specify the refresh ratio in ms'
+}).argv
 
-if (!argv.macro) {
+if (argv.debug) {
+  try {
+    execDebug(argv.debug)
+  } catch (error) {
+    console.error(`Error during debug session: ${error}`)
+  }
+} else if (!argv.macro) {
   yargs.showHelp()
   process.exit(1)
 }
@@ -19,6 +35,40 @@ async function run() {
   const obj = await scriptLoader.load(targetPath)
   console.log(obj)
   macroController.executeMacro(obj)
+}
+
+async function execDebug(refreshRate) {
+  // setup ctrl+c hook
+  if(process.platform === "win32") {
+    readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    }).on('SIGINT', () => {
+      process.emit('SIGINT')
+    })
+  }
+
+  let print = true
+  let rfrshRate = refreshRate || 1000
+  console.log(`Using refresh rate: ${rfrshRate}`)
+
+  process.on('SIGINT', () => {
+    print = false
+  })
+
+  function printDebug() {
+    setTimeout(() => {
+      if (print) {
+        runDebug(printDebug)
+      } else {
+        console.log('Stopping Debug Session.')
+        process.exit(0)
+      }
+    }, rfrshRate)
+  }
+
+  printDebug()
+
 }
 
 run()
